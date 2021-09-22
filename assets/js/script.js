f4327866d0868079d7825373653a1534bddce354
Vitor Afonso <vittokm@hotmail.com>
2021-09-22 16:57:39 +0200

    added examples of destructing arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,56 @@ let car = {
     }
 };
 
+
+
 let {brand:marca, make:modelo, owners:{first = "desconhecido"}, owners:{second:segundoDono}, yearsOld:idade} = car;
 
-//here we are binding the variable to the object, so that we can call the variable(function reference) from the outside the object.
+//here we bind the variable to the object, so that we can call the variable(function reference) from the outside the object.
 idade = idade.bind(car);
 
 console.log(`O segundo dono do ${marca} ${modelo} foi o ${segundoDono}, já o primeiro é ${first}. O carro tem ${idade()} anos de idade`);
+
+
+
+
+
+//Destructuring directly in function parameter
+
+function getBrandAndMake({brand, make}) {
+    return `${brand} ${make}`;
+}
+
+let res = getBrandAndMake(car);
+
+console.log(res);
+
+
+
+
+
+
+// DESTRUCTURING ARRAYS
+
+// destructuring, 
+// defining just the variables i need, 
+// assigning a default value to an inexistent item
+
+let myArray = ['Vitor', 'Afonso',];
+
+let [myName, , age = 77] = myArray;
+
+console.log(`${myName} ${age}`);
+
+
+// destructuring via function
+
+
+function create() {
+    return [3, 7, 11, 15, 20, 23, 25];
+}
+let [ , , , d, , f] = create();
+
+console.log(d, f);
+
+
 
